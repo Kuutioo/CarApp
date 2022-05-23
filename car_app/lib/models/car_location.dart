c282ab42f4362741edd5159a107debd560439639
Arttu Hakkarainen <ape.hakkarainen@gmail.com>
2022-05-23 14:02:15 +0200

    Can now get car location

---
+++ car_app/lib/models/car_location.dart
@@ -0,0 +1,11 @@
+class CarLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  const CarLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+}
