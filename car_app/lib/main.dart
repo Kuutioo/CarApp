10aa75e37c66a98ea9c95bfb119878310e7579ed
Arttu Hakkarainen <ape.hakkarainen@gmail.com>
2022-05-23 12:06:48 +0200

    Added pages

--- car_app/lib/main.dart
+++ car_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import './pages/cars_page.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CarsPage(),
     );
   }
 }
---
+++ car_app/lib/pages/cars_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CarsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
