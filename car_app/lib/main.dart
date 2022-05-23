e981d4535f0fd51383096639fc93620bcac3f2c0
Janu <ante.sarre@gmail.com>
2022-05-23 09:38:04 +0200

    ok

--- car_app/lib/main.dart
+++ car_app/lib/main.dart
@@ -21,7 +21,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Hello!'),
+      child: Text('ok'),
     );
   }
 }
