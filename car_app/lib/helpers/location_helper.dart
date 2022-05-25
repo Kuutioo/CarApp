3b7a74c7b3b124be90241192a6bcdc6a87f8669e
Janu <ante.sarre@gmail.com>
2022-05-25 11:40:31 +0200

    fixed merge

--- car_app/lib/helpers/location_helper.dart
+++ car_app/lib/helpers/location_helper.dart
@@ -8,6 +8,11 @@ const MAPBOX_API_KEY =
     'pk.eyJ1IjoiYXBpbmFtZXN0YXJpMTIiLCJhIjoiY2wzaWt5NGMzMDAyNjNicXkwMnNocmxlMiJ9.qRmY8u7p2y0bc2ZWnzTrJg';
 
 class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double? latitude, double? longitude}) {
+    return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,60/600x300?access_token=$MAPBOX_API_KEY';
+  }
+
   static Future<String> getPlaceAddress(double lat, double lng) async {
     final url = Uri.parse(
         'https://api.mapbox.com/geocoding/v5/mapbox.places/$lng,$lat.json?limit=1&access_token=$MAPBOX_API_KEY');
--- car_app/lib/pages/cars_page.dart
+++ car_app/lib/pages/cars_page.dart
@@ -1,16 +1,124 @@
 // ignore_for_file: deprecated_member_use, use_key_in_widget_constructors
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:location/location.dart';
 
+import '../helpers/location_helper.dart';
 import '../widgets/main_drawer.dart';
 import '../widgets/car_item.dart';
 
-class CarsPage extends StatelessWidget {
+class CarsPage extends StatefulWidget {
+  @override
+  State<CarsPage> createState() => _CarsPageState();
+}
+
+class _CarsPageState extends State<CarsPage> {
+  String? titleInput;
+
+  String? imageInput;
+
+  String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: locData.latitude,
+      longitude: locData.longitude,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  void startAddNewCar(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return Card(
+          elevation: 5,
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Car name'),
+                  onChanged: (val) => titleInput = val,
+                ),
+                TextField(
+                  keyboardType: TextInputType.url,
+                  decoration: const InputDecoration(labelText: 'Image url'),
+                  onChanged: (val) => imageInput = val,
+                ),
+                SizedBox(
+                  height: 150,
+                  child: Container(
+                    margin: const EdgeInsets.all(15),
+                    height: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: _previewImageUrl == null
+                        ? const Text(
+                            'No Location Chosen',
+                            textAlign: TextAlign.center,
+                          )
+                        : Image.network(
+                            _previewImageUrl!,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.location_on,
+                      ),
+                      label: const Text('Current Location'),
+                      onPressed: _getCurrentUserLocation,
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.map,
+                      ),
+                      label: const Text('Select on Map'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  child: const Text('Add car'),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cars'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              startAddNewCar(context);
+            },
+          ),
+        ],
       ),
       drawer: MainDrawer(),
       body: StreamBuilder(
